691251a332eabe2a3e697229d7513af876ecd8e9
Sindhu <sindhukarigerr@email.com>
2026-04-10 19:39:20 +0530

    Added Task 3: JWT authentication and role-based authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/test'), 
-    UserModule,
+    UserModule, AuthModule,
   ],
 })
 export class AppModule {}
--- src/user.entity.ts
+++ src/user.entity.ts
@@ -1,12 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-  @Prop()
-  name: string;
 
-  @Prop()
-  email: string;
+  @Prop({ required: true })
+  name!: string;   // ✅ add !
+
+  @Prop({ required: true, unique: true })
+  email!: string;  // ✅ add !
+
+  @Prop({ required: true })
+  password!: string;   
+
+  @Prop({ required: true, enum: ['doctor', 'patient'] })
+  role!: 'doctor' | 'patient';       
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
